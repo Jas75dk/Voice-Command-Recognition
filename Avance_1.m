6b57f4406b511988e62489f5435ce881ea12dfa6
Jas75dk <149320258+Jas75dk@users.noreply.github.com>
2026-04-21 19:12:46 -0500

    Add files via upload

---
+++ Avance_1.m
@@ -0,0 +1,11 @@
+%% *PROCESAMIENTO DE SEÑALES E IMÁGENES DIGITALES*
+%% IEE239 2026-01
+% Nombres - Codigo:
+% Jason Alexis Guillermo Sandoval - 20212711
+% 
+% 
+
+url = 'http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz';
+filename = 'speech_commands.tar.gz';
+
+websave(filename, url);
